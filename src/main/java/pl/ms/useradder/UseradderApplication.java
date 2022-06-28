7f0bc486fad5dcb3a015949ddf8e2de5a32c2b9b
Magda <m.stebel@mailplus.pl>
2022-06-28 19:27:34 +0200

    Stworzenie projektu

---
+++ src/main/java/pl/ms/useradder/UseradderApplication.java
@@ -0,0 +1,13 @@
+package pl.ms.useradder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseradderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UseradderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/ms/useradder/UseradderApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ms.useradder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UseradderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
